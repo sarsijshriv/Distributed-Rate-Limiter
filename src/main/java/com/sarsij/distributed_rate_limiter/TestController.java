b601db6675999e0cedbd4a09ac69faf93999f497
sarsijshriv <47062704+sarsijshriv@users.noreply.github.com>
2026-05-03 10:16:08 +0530

    Initial Spring boot setup with test endpoint

---
+++ src/main/java/com/sarsij/distributed_rate_limiter/TestController.java
@@ -0,0 +1,13 @@
+package com.sarsij.distributed_rate_limiter;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String testSpring(){
+        return "OK";
+    }
+}
